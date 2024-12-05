5564376f24c5bd6c5620607d13ac6d21e9ad0fd1
pratham-107 <spratham845@gmail.com>
2024-12-05 19:25:41 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// console.log("hello world");
+
+// for(let i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+// const a =10;
+// const b = 20;
+// const sum = a + b;
+// console.log(sum);
+
+
+// const number = parseInt(prompt('enter the number : '));
+// let sum = 0;
+
+// for(let i=0; i<=number; i++){
+//     sum = sum + i;
+//     console.log('sum of n natural number is: ' +  sum);
+// }
+
+
+
+// let a = prompt('Enter the value of a: ');
+// let b = prompt('Enter the value of b: ');
+// let temp;
+
+// temp = a; // Store the value of a in temp
+// a = b;    // Assign the value of b to a
+// b = temp; // Assign the value of temp (original a) to b
+
+// console.log('Value of a after swapping: ' + a);
+// console.log('Value of b after swapping: ' + b);
+
+
+
+
+
+
+// a = 5;
+// b = 10;
+
+// console.log(a,b);
+
+// a = a + b;                      // without using variable
+// b = a - b;
+// a = a - b;
+
+// // [a,b] = [b,a];                   //array deconstruction
+
+// console.log(a,b);
+
+
+
+
