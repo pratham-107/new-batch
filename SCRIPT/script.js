b261ebba0419ca698cb046ed5ef8adb46a14394c
pratham-107 <spratham845@gmail.com>
2024-12-26 15:01:25 +0530

    initial commit

---
+++ SCRIPT/script.js
@@ -0,0 +1,29 @@
+//FINDING SINGLE NUMBER//
+// let n = 38;
+// while(n >= 10){
+//     let sum = 0;
+//     while(n != 0){
+//         let rem = n % 10;
+//         sum = sum + rem;
+//         n = Math.floor(n/10);
+//     }
+//     n = sum;
+// }
+// console.log(n);
+
+
+
+//circle diving//
+// let n = 4;
+// if(n == 1){
+//     console.log(0);
+// }else{
+//     if(n % 2 == 0){
+//         console.log(n/2);
+//     }else{
+//         console.log(n);
+//     }
+// }
+
+
+
